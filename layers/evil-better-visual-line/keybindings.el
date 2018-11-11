75f4edb77dc7de284a89b2e747b6e9bbdec1cc2e
YourFin <d.junkpobox+github@gmail.com>
2018-11-11 14:41:46 -0600

    Add evil-better-visual-line

---
+++ layers/evil-better-visual-line/keybindings.el
@@ -0,0 +1,2 @@
+(with-eval-after-load 'evil-better-visual-line
+  (evil-better-visual-line-on))
---
+++ layers/evil-better-visual-line/packages.el
@@ -0,0 +1,5 @@
+(defconst evil-better-visual-line-packages
+  '(evil-better-visual-line))
+
+(defun evil-better-visual-line/init-evil-better-visual-line ()
+  (use-package evil-better-visual-line))
