ce7555b8bbca61df1f4d60e3f2cb54945b94996a
Patrick Nuckolls <nuckolp@amazon.com>
2019-06-03 10:10:20 -0700

    Add company laptop support

---
+++ lisp/systems/init-ud0485683217452.ant.amazon.com.el
@@ -0,0 +1,14 @@
+;;; init-ud0485683217452.ant.amazon.com.el --- laptop-init  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Patrick Nuckolls
+
+;; Author: nuckolp <nuckolp@ud0485683217452.ant.amazon.com>
+;; Keywords: hardware
+
+(setq dotspacemacs-default-font '("Fira Code"
+                                  :size 12
+                                  :weight normal
+                                  :width normal
+                                  :powerline-scale 1.1))
+
+(provide 'init-ud0485683217452.ant.amazon.com)
